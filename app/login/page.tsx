46d88f95d3e75ffb0121c39c587f96bd8d5bfb03
jangjaeH <pvcgrdo@gmail.com>
2026-01-09 16:24:29 +0900

    팝업 임시

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useState } from 'react';
-import { Form, Input, Button } from 'antd';
+import { Form, Input, Button, Modal } from 'antd';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { useRouter } from 'next/navigation';
 import Cookies  from 'js-cookie';
@@ -8,7 +8,17 @@ import CustomAlert from '@/components/alert';
 export default function LoginPage() {
 
     const [message, setMessage] = useState<string | null>(null);
+    const [ismodalOpen, setIsModalOpen] = useState(false);
     const router = useRouter();
+
+    const showModal = () => {
+        setIsModalOpen(true);
+    }
+
+    const showModalClose = () => {
+        setIsModalOpen(false);
+    }
+
 
     const onFinish = async (values: any) => {
         const params = { ...values, action: 'login' };
@@ -69,7 +79,34 @@ export default function LoginPage() {
                         Log in
                     </Button>
                 </Form.Item>
+                <Button type="link" onClick={showModal}>회원가입</Button>
             </Form> 
+            <Modal title="Login Info" open={ismodalOpen} onOk={showModalClose} onCancel={showModalClose}>
+                <Form
+                    name="newid_form"
+                >
+                    <Form.Item
+                        name="newid_usercode"
+                    >
+                        <Input placeholder="ID" />
+                    </Form.Item>
+                    <Form.Item
+                        name="newid_username"
+                    >
+                        <Input placeholder="이름" />
+                    </Form.Item>
+                    <Form.Item
+                        name="newid_password"
+                    >
+                        <Input placeholder="PassWord" />
+                    </Form.Item>
+                    <Form.Item
+                        name="newid_password_confirm"
+                    >
+                        <Input placeholder="PassWord Confirm" />
+                    </Form.Item>
+                </Form>
+            </Modal>
         </div>
     )
 } 
